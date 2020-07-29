e5c7a9b5642013394ddd92ca70bf200074f79bce
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-07-30 00:09:34 +0530

    Initial commit

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return(
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <h4>Be a part of our Community</h4>
+          <p>The title and other copy in the introduction grab users and make them feel compelled to action, but you should also add an attractive illustration or background to ent </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
